3ae00758c1edb544f26de4efdddd19c79bd2ee68
Vlas <vlas@MacBook-Pro-Vlas.local>
2020-12-05 13:02:39 +0300

    init

---
+++ first.py
@@ -0,0 +1,18 @@
+FriendlyInterface = "The heaviest coin is"
+
+print("Don't forget to put cursor on the line of console!")
+
+first = int(input("Write the weight of the first coin: "))
+second = int(input("Write the weight of the second coin: "))
+third = int(input("Write the weight of the third coin: "))
+
+if second > first and second > third:
+    print(FriendlyInterface, "second, its weight is", second)
+
+if first > second and first > third:
+    print(FriendlyInterface, "first, its weight is", first)
+
+if third > first and third > second:
+    print(FriendlyInterface, "third, its weight is", third)
+
+print("Please, push Ctrl + R to compare another three coins")
