1b651ba56e33d3a293b616433313aebbc3ad874b
Vlas Vigilyanskiy <vlas.vigilyanskiy@gmail.com>
2020-12-08 02:21:21 +0300

    Finished Begin 28 task.

---
+++ Tasks/Begin28.py
@@ -0,0 +1,10 @@
+a = int(input("Write the A: "))
+a2 = a ** 2
+a5 = a ** 5
+
+print("A =", a)
+print("A2 =", a2)
+print("A3 =", a2 * a)
+print("A5 =", a5)
+print("A10 =", a5 * a5)
+print("A15 =", a5 * a5 * a5)
