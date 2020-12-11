08cc3489bc78528b56218d8ea81a1839677873fa
Vlas Vigilyanskiy <vlas.vigilyanskiy@gmail.com>
2020-12-11 15:40:18 +0300

    Finished Boolean 7 task.

---
+++ Tasks/Boolean7.py
@@ -0,0 +1,8 @@
+a = int(input("Write the A: "))
+b = int(input("Write the B: "))
+c = int(input("Write the C: "))
+
+if a < b < c or a > b > c:
+    print("The B is the middle number. True!")
+else:
+    print("The B is not the middle number. False!")
