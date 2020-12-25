432b88c399fcbf59b20414f41a6523e4df1a8314
Vlas Vigilyanskiy <vlas.vigilyanskiy@gmail.com>
2020-12-25 17:31:23 +0300

    Finished Task3 and Task4(TASK3 AWESOME!).

---
+++ Tasks/2020_12_19/Task3.py
@@ -0,0 +1,72 @@
+from random import randint
+
+
+def tank1_shoot():
+    reach_or_not = randint(1, 3)
+    fire_or_not = randint(1, 2)
+
+    if reach_or_not == 2:
+        if fire_or_not == 2:
+            tank2["health"] -= tank1["fire"]
+            tank2["health"] -= 1
+            print("Reach and bonus, tank2 is burning! Tank2 health:", tank2["health"])
+        else:
+            tank2["health"] -= tank1["fire"]
+            print("Reach! Tank2 health:", tank2["health"])
+    elif reach_or_not == 3:
+        if tank2["armor"] > 0:
+            tank2["armor"] -= tank1["fire"]
+            print("Reach, but you got the armor! Tank2 armor:", tank2["armor"])
+        else:
+            tank2["health"] -= tank1["fire"]
+            print("Reach, but you got the armor, but the armor is broken, so you got the tank2 itself! "
+                  "Tank2 health:", tank2["health"])
+    else:
+        print("Miss! Tank2 health:", tank2["health"])
+
+
+def tank2_shoot():
+    reach_or_not2 = randint(1, 3)
+    fire_or_not2 = randint(1, 2)
+
+    if reach_or_not2 == 2:
+        if fire_or_not2 == 2:
+            tank1["health"] -= tank2["fire"]
+            tank1["health"] -= 1
+            print("Reach and bonus, tank1 is burning! Tank1 health:", tank1["health"])
+        else:
+            tank1["health"] -= tank2["fire"]
+            print("Reach! Tank1 health:", tank1["health"])
+    elif reach_or_not2 == 3:
+        if tank1["armor"] > 0:
+            tank1["armor"] -= tank2["fire"]
+            print("Reach, but you got the armor! Tank1 armor:", tank1["armor"])
+        else:
+            tank1["health"] -= tank1["fire"]
+            print("Reach, but you got the armor, but the armor is broken, so you got the tank1 itself! "
+                  "Tank1 health:", tank1["health"])
+    else:
+        print("Miss! Tank1 health:", tank1["health"])
+
+
+tank1 = {"armor": 5, "health": 15, "speed": 15, "fire": 2, "steer": 2}
+tank2 = {"armor": 10, "health": 20, "speed": 5, "fire": 3, "steer": 1}
+
+while True:
+    if tank1["health"] < 0:
+        print("The winner is tank2!")
+        exit(0)
+    elif tank2["health"] < 0:
+        print("The winner is tank1!")
+        exit(0)
+    else:
+        pass
+
+    who_shoots = input("Put in the tank that is shooting: ")
+
+    if who_shoots == "tank1":
+        tank1_shoot()
+    elif who_shoots == "tank2":
+        tank2_shoot()
+    else:
+        print("What did you write?")
---
+++ Tasks/2020_12_19/Task4.py
@@ -0,0 +1,9 @@
+def func_1():
+    print("Your number in second power is", number ** 2)
+    print("Your number in third power is", number ** 3)
+    print("Your number in fourth power is", number ** 4)
+
+
+number = int(input("Put in the number: "))
+
+print(func_1())
